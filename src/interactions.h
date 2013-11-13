ce946abf3ab93f4abf329a67d3b96fca8fd2a875
lazydog616 <xiaozeng@seas.upenn.edu>
2013-11-13 00:52:46 -0500

    cuda ray tracer with basic phong model and soft shadow

---
+++ src/interactions.h
@@ -0,0 +1,55 @@
+// CIS565 CUDA Raytracer: A parallel raytracer for Patrick Cozzi's CIS565: GPU Computing at the University of Pennsylvania
+// Written by Yining Karl Li, Copyright (c) 2012 University of Pennsylvania
+// This file includes code from:
+// Yining Karl Li's TAKUA Render, a massively parallel pathtracing renderer: http://www.yiningkarlli.com
+
+#ifndef INTERACTIONS_H
+#define INTERACTIONS_H
+
+#include "intersections.h"
+
+//forward declaration
+__host__ __device__ glm::vec3 getRandomDirectionInSphere(float xi1, float xi2);
+__host__ __device__ glm::vec3 calculateRandomDirectionInHemisphere(glm::vec3 normal, float xi1, float xi2);
+
+//LOOK: This function demonstrates cosine weighted random direction generation in a sphere!
+__host__ __device__ glm::vec3 calculateRandomDirectionInHemisphere(glm::vec3 normal, float xi1, float xi2) {
+    
+    //crucial difference between this and calculateRandomDirectionInSphere: THIS IS COSINE WEIGHTED!
+    
+    float up = sqrt(xi1); // cos(theta)
+    float over = sqrt(1 - up * up); // sin(theta)
+    float around = xi2 * TWO_PI;
+    
+    //Find a direction that is not the normal based off of whether or not the normal's components are all equal to sqrt(1/3) or whether or not at least one component is less than sqrt(1/3). Learned this trick from Peter Kutz.
+    
+    glm::vec3 directionNotNormal;
+    if (abs(normal.x) < SQRT_OF_ONE_THIRD) {
+      directionNotNormal = glm::vec3(1, 0, 0);
+    } else if (abs(normal.y) < SQRT_OF_ONE_THIRD) {
+      directionNotNormal = glm::vec3(0, 1, 0);
+    } else {
+      directionNotNormal = glm::vec3(0, 0, 1);
+    }
+    
+    //Use not-normal direction to generate two perpendicular directions
+    glm::vec3 perpendicularDirection1 = glm::normalize(glm::cross(normal, directionNotNormal));
+    glm::vec3 perpendicularDirection2 = glm::normalize(glm::cross(normal, perpendicularDirection1));
+    
+    return ( up * normal ) + ( cos(around) * over * perpendicularDirection1 ) + ( sin(around) * over * perpendicularDirection2 );
+    
+}
+
+//TODO: IMPLEMENT THIS FUNCTION
+//Now that you know how cosine weighted direction generation works, try implementing non-cosine (uniform) weighted random direction generation.
+//This should be much easier than if you had to implement calculateRandomDirectionInHemisphere.
+__host__ __device__ glm::vec3 getRandomDirectionInSphere(float xi1, float xi2) {
+  //TODO:: find out the parameter xi1 xi2 mean and what this function really do.
+	thrust::default_random_engine rng(hash(2));
+    thrust::uniform_real_distribution<float> u(-0.5,0.5);
+	glm::vec3 dire = glm::vec3(u(rng), u(rng), u(rng));//to check if this is the right way to use random func
+	dire = glm::normalize(dire);
+    return dire;//stay in this form for now, don't know the meaning
+}
+
+#endif
